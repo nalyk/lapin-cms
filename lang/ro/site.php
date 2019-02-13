3d08c9e485f7efbdeed0eb45868f3dcee35baa61
nalyk <dev.ungheni@gmail.com>
2019-02-13 16:38:58 +0200

    Update

---
+++ lang/ro/site.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dashboard' => 'Panou',
+    'management' => 'Management',
+    'new_s_m' => 'nou',
+    'news_s_f' => 'nouă',
+    'list' => 'listă',
+    'yes' => 'da',
+    'no' => 'nu',
+    'synchronization' => 'sincronizare',
+    'configuration' => 'configurare',
+    'dpd_objects' => 'DPD Objects'
+];
